73bd590b7ae8f285c313e106c43008dc2e239346
liuwei <liuwei@top-list.com.cn>
2022-09-24 14:08:25 +0800

    post

--- build.sh
+++ build.sh
@@ -2,19 +2,22 @@
 
 set -eux
 
+BUILD_NAME='ops-note'
+BUILD_VERSION='liuwei.co'
+
 hugo -D
 
-docker build -t ops-note:v1 -f Dockerfile .
+docker build --no-cache -t $BUILD_NAME:$BUILD_VERSION -f Dockerfile .
 
-docker images | grep 'ops-note'
+docker images | grep $BUILD_VERSION
 
-IMAGETAG=$(docker images | grep 'ops-note' | head -n 1 | awk '{print $3}')
+IMAGETAG=$(docker images | grep $BUILD_VERSION | head -n 1 | awk '{print $3}')
 
 echo $IMAGETAG
 
-docker tag $IMAGETAG toplist-registry.cn-shanghai.cr.aliyuncs.com/88/ops-note:v1
+docker tag $IMAGETAG toplist-registry.cn-shanghai.cr.aliyuncs.com/88/$BUILD_NAME:$BUILD_VERSION
 
-docker push toplist-registry.cn-shanghai.cr.aliyuncs.com/88/ops-note:v1
+docker push toplist-registry.cn-shanghai.cr.aliyuncs.com/88/$BUILD_NAME:$BUILD_VERSION
 
 kubectl rollout restart deploy ops-note -n pwk
 
